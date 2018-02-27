87569c4bf45db684b07c985b75165ed067945b7f
sdlylzhr@163.com <sdlylzhr@163.com>
2018-02-27 11:58:14 +0800

    change property name

--- VideoStream/ViewController.h
+++ VideoStream/ViewController.h
@@ -11,7 +11,7 @@
 @interface ViewController : UIViewController
 
 @property(nonatomic, retain) UITextField *textField;
-@property(nonatomic, strong) UILabel *myLabel;
+@property(nonatomic, strong) UILabel *label;
 
 @end
 
